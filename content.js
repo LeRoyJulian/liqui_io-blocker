789d22e0e5db06ce665c93e97b5d4dddd5bbc55f
Julian Vogin <julian@bartounet.fr>
2017-08-22 03:20:13 +0200

    Improvements

--- content.js
+++ content.js
@@ -1,33 +1,44 @@
+function checker(value) {
+   var muted = [
+      'blammas',
+      'etherisreal',
+      'ram123',
+      'imab1',
+      'TheWanderer',
+      'nehanino',
+      'BanaanBakje',
+      'bryan',
+      'bobfish',
+      'toomuchfun',
+      'etherisreaI',
+      'kosarkas',
+      'ChronoTr',
+      'thefuturist133',
+      'offermann515',
+      'WowlookatZRXarbthat',
+      'eggwagon'
+   ];
+  for (var i = 0; i < muted.length; i++) {
+    if (value.indexOf(muted[i]) !== -1) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function filter() {
-  var messages = document.getElementsByClassName('message-text');
-    for (var i = 0; i < messages.length; i++) {
+   var messages = document.getElementsByClassName('message-text');
+   for (var i = 0; i < messages.length; i++) {
       if (messages[i] && messages[i].innerHTML) {
-        var message = messages[i].getElementsByTagName('a');
-        for (var j = 0; j < message.length; j++) {
-          if (
-            message[j].innerHTML.indexOf('blammas') !== -1 ||
-            message[j].innerHTML.indexOf('etherisreal') !== -1 ||
-            message[j].innerHTML.indexOf('ram123') !== -1 ||
-            message[j].innerHTML.indexOf('imab1') !== -1 ||
-            message[j].innerHTML.indexOf('TheWanderer') !== -1 ||
-            message[j].innerHTML.indexOf('nehanino') !== -1 ||
-            message[j].innerHTML.indexOf('BanaanBakje') !== -1 ||
-            message[j].innerHTML.indexOf('bryan') !== -1 ||
-            message[j].innerHTML.indexOf('bobfish') !== -1 ||
-            message[j].innerHTML.indexOf('toomuchfun') !== -1 ||
-            message[j].innerHTML.indexOf('etherisreaI') !== -1 ||
-            message[j].innerHTML.indexOf('kosarkas') !== -1 ||
-            message[j].innerHTML.indexOf('ChronoTr') !== -1 ||
-            message[j].innerHTML.indexOf('thefuturist133') !== -1 ||
-            message[j].innerHTML.indexOf('offermann515') !== -1 ||
-            message[j].innerHTML.indexOf('WowlookatZRXarbthat') !== -1 ||
-            message[j].innerHTML.indexOf('eggwagon') !== -1
-          )
-            messages[i].remove();
-          }
-        }
+         var message = messages[i].getElementsByTagName('a');
+         for (var j = 0; j < message.length; j++) {
+            if (checker(message[j].innerHTML)) {
+               messages[i].remove();
+            }
+         }
       }
-    setTimeout(function() { filter() }, 1500);
+   }
+   setTimeout(function() { filter() }, 1500);
 }
 
 filter();
