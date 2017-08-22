05942873bb4f9f59daff00fa66ac20494f47b10f
Julian Vogin <julian@bartounet.fr>
2017-08-22 03:43:23 +0200

    Muted Asians & Blocked another asshole

--- content.js
+++ content.js
@@ -16,7 +16,9 @@ function checker(value) {
       'thefuturist133',
       'offermann515',
       'WowlookatZRXarbthat',
-      'eggwagon'
+      'eggwagon',
+      'heroesjourney',
+      'b12345'
    ];
   for (var i = 0; i < muted.length; i++) {
     if (value.indexOf(muted[i]) !== -1) {
@@ -28,11 +30,12 @@ function checker(value) {
 
 function filter() {
    var messages = document.getElementsByClassName('message-text');
+   var regex = new RegExp("/[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/");
    for (var i = 0; i < messages.length; i++) {
       if (messages[i] && messages[i].innerHTML) {
          var message = messages[i].getElementsByTagName('a');
          for (var j = 0; j < message.length; j++) {
-            if (checker(message[j].innerHTML)) {
+            if (checker(message[j].innerHTML) || regex.test(message[j].innerHTML)) {
                messages[i].remove();
             }
          }
