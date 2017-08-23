54c2ca44a4b5b08e791f8d7ce1b96adb31a3d25f
Julian Vogin <julian@bartounet.fr>
2017-08-24 01:04:41 +0200

    Fix

--- content.js
+++ content.js
@@ -17,12 +17,12 @@ function checker(value) {
 
 function filter() {
    var messages = document.getElementsByClassName('message-text');
-   var regex = new RegExp("/[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/");
+   // var regex = new RegExp("/[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/");
    for (var i = 0; i < messages.length; i++) {
       if (messages[i] && messages[i].innerHTML) {
          var message = messages[i].getElementsByTagName('a');
          for (var j = 0; j < message.length; j++) {
-            if (checker(message[j].innerHTML) || regex.test(message[j].innerHTML)) {
+            if (checker(message[j].innerHTML)) {
                messages[i].remove();
             }
          }
